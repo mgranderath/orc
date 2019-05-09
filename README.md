e493e3b89384a2ee1ed76aedba371001ba2ba3d8
Malte Granderath <m.granderath@jacobs-university.de>
2019-05-09 11:22:51 +0200

    Documentation & Cleanup

--- README.md
+++ README.md
@@ -15,12 +15,24 @@ datastore.
 To add YANG modules for OpenWrt they have to go through some pre-processing. This is
 what the `main.py` script does.
 
-1. Convert the YANG modules to be included to YIN and put them in one folder, i.e. `/yin`
+### Annotations
+
+Before YANG modules can be used with this implementation they have to be
+annotated with the extensions provided in `/yang/openwrt-uci-extension.yang`.
+An example of an annotated module is `/yang/restconf-example.yang`
+
+### Script
+
+1. Convert the YANG modules to be included to YIN and put them in one folder, i.e. `/yin`.
+   For example with [`pyang`](https://github.com/mbj4668/pyang)
+   ```console
+   pyang -f yin ./yang/restconf-example.yang -p ./yang -o ./yin/restconf-example.yin 
+   ```
 2. Run the `main.py` script
    ```console
-   python3 main.py -y ./yin -o ./generated restconf-example.yin ...
+   python3 ./yin2json/yin2json.py -y ./yin -o ./generated ./yin/restconf-example.yin ...
    ```
-   This converts the YIN files and generates a `.h` file that has to be included in `/src/generated/yang.h`
+   This converts the YIN files and generates a `.h` file in `./generated` that has to be included in `/src/generated/yang.h`
 
 ## Building
 
@@ -35,3 +47,19 @@ script on OpenWrt
 ## Architecture
 
 ![Architecture](docs/resources/Architecture.png)
+
+The binary that is compiled is a CGI script that can be used with any
+CGI enabled web server but on OpenWrt the uHTTP server is recommended.
+
+To utilize the script with the uHTTP server the following steps have to
+be followed:
+
+1. Install uHTTP with
+   ```console
+   opkg update
+   opkg install uhttpd
+   ```
+2. Configure uHTTPd ([Documentation](https://openwrt.org/docs/guide-user/services/webserver/uhttpd))
+3. Make sure the `http_keepalive` option in `/etc/config/uhttpd` is set to `0`
+4. Copy the script into the `/www/cgi-bin/` folder
+5. Now you should be able to make requests at the `<IP>/cgi-bin/restconf` URL
--- yin2json/yin2json.py
+++ yin2json/yin2json.py
@@ -3,119 +3,57 @@ import json
 import os
 
 import xmltodict
+from mako.lookup import TemplateLookup
 from mako.template import Template
 
-c = Template("""
-<%! import json %>
-#ifndef _YANG_H
-#define _YANG_H
-#include <stddef.h>
-#include <json-c/json.h>
-#include <string.h>
+dirname = os.path.dirname(os.path.realpath(__file__))
+mylookup = TemplateLookup(directories=[os.path.join(dirname, "./template")])
+header_file = Template(filename=os.path.join(dirname, "./template/yang.h.templ"), lookup=mylookup)
 
-struct map_str2str{
-  char *key;
-  char *str;
-};
-typedef struct map_str2str map_str2str;
-
-static const map_str2str modulemap[] = {
-    % for index, (name, content) in enumerate(modules):
-    % if index + 1 == len(modules):
-    {"${name}", ${json.dumps(json.dumps(content))}}
-    % else:
-    {"${name}", ${json.dumps(json.dumps(content))}},
-    %endif
-    % endfor
-};
-
-static const map_str2str yang2regex[] = {
-    % for index, (key, value) in enumerate(types.items()):
-    % if index + 1 == len(types.items()):
-    {"${key}", ${json.dumps(json.dumps(value))}}
-    % else:
-    {"${key}", ${json.dumps(json.dumps(value))}},
-    %endif
-    % endfor
-};
-
-enum yang_type {
-  NONE,
-  BINARY,
-  BITS,
-  BOOLEAN,
-  DECIMAL_64,
-  EMPTY,
-  ENUMERATION,
-  IDENTITY_REF,
-  INSTANCE_IDENTIFIER,
-  INT_8,
-  INT_16,
-  INT_32,
-  INT_64,
-  LEAF_REF,
-  STRING,
-  UINT_8,
-  UINT_16,
-  UINT_32,
-  UINT_64,
-  UNION
-};
-typedef enum yang_type yang_type;
-
-struct map_str2yang_type {
-  char* str;
-  yang_type type;
-};
-typedef struct map_str2yang_type map_str2yang_type;
-
-static const map_str2yang_type str2yang_type[] = {
-    {"binary", BINARY},
-    {"bits", BITS},
-    {"boolean", BOOLEAN},
-    {"decimal64", DECIMAL_64},
-    {"emtpy", EMPTY},
-    {"enumeration", ENUMERATION},
-    {"identityref", IDENTITY_REF},
-    {"instance-identifier", INSTANCE_IDENTIFIER},
-    {"int8", INT_8},
-    {"int16", INT_16},
-    {"int32", INT_32},
-    {"int64", INT_64},
-    {"leafref", LEAF_REF},
-    {"string", STRING},
-    {"uint8", UINT_8},
-    {"uint16", UINT_16},
-    {"uint32", UINT_32},
-    {"uint64", UINT_64},
-    {"union", UNION}
-};
-
-struct json_object* yang_module_exists(char* module);
-yang_type str_to_yang_type(const char* str);
-const char* yang_for_type(const char* type);
-
-#endif
-""")
-
-importedModules = {}
-importedTypes = {}
-openwrt_prefix = ""
 types = {}
-allowed_types = {
-    "string": True,
-    "int8": True,
-    "int16": True,
-    "int32": True,
-    "int64": True,
-    "uint8": True,
-    "uint16": True,
-    "uint32": True,
-    "uint64": True,
-    "binary": True,
-    "boolean": True,
-    "decimal64": True
+ALLOWED_TYPES = {
+    "string": "STRING",
+    "int8": "INT_8",
+    "int16": "INT_16",
+    "int32": "INT_32",
+    "int64": "INT_64",
+    "uint8": "UINT_8",
+    "uint16": "UINT_16",
+    "uint32": "UINT_32",
+    "uint64": "UINT_64",
+    "binary": "BINARY",
+    "boolean": "BOOLEAN",
+    "decimal64": "DECIMAL_64"
 }
+
+
+class Imported:
+    def __init__(self):
+        self.modules = {}
+        self.types = {}
+        self.openwrt_prefix = ""
+
+    def set_openwrt_prefix(self, prefix):
+        self.openwrt_prefix = prefix
+
+    def add_module(self, prefix, name):
+        self.modules[prefix] = name
+
+    def get_modules(self):
+        return self.modules
+
+    def get_module(self, prefix):
+        if prefix not in self.modules:
+            return ""
+        return self.modules[prefix]
+
+    def add_type(self, module_name, content):
+        if module_name not in self.types:
+            self.types[module_name] = []
+        self.types[module_name].append(content)
+
+    def get_types(self):
+        return self.types
 
 
 def convert_yin_to_json(data):
@@ -127,44 +65,42 @@ def range_allowed(name):
     return name in ["int8", "int16", "int32", "int64", "uint8", "uint16", "uint32", "uint64"]
 
 
-def handle_import(js):
-    global openwrt_prefix
+def handle_import(js, imported):
     if isinstance(js, dict):
         name = js["@module"]
         prefix = js["prefix"]["@value"]
-        importedModules[prefix] = name
+        imported.add_module(prefix, name)
         if name == "openwrt-uci-extension":
-            openwrt_prefix = prefix
+            imported.set_openwrt_prefix(prefix)
     elif isinstance(js, list):
         for item in js:
-            handle_import(item)
+            handle_import(item, imported)
 
 
-def extract_uci_statements(generated, key, value):
-    if key == openwrt_prefix + ":uci-package":
+def extract_uci_statements(generated, key, value, imported):
+    if key == imported.openwrt_prefix + ":uci-package":
         generated["uci-package"] = value["@name"]
-    if key == openwrt_prefix + ":uci-section":
+    if key == imported.openwrt_prefix + ":uci-section":
         generated["uci-section"] = value["@name"]
-    if key == openwrt_prefix + ":uci-option":
+    if key == imported.openwrt_prefix + ":uci-option":
         generated["uci-option"] = value["@name"]
-    if key == openwrt_prefix + ":uci-section-type":
+    if key == imported.openwrt_prefix + ":uci-section-type":
         generated["uci-section-type"] = value["@name"]
 
 
-def extract_type_statements(generated, key, value):
+def extract_type_statements(generated, key, value, imported):
     type_name = value["@name"]
     if ":" in type_name:
         split = type_name.split(":", 1)
-        if split[0] not in importedModules:
-            raise Exception("Using type from module that is not defined {}".format(split[0]))
-        if importedModules[split[0]] not in importedTypes:
-            importedTypes[importedModules[split[0]]] = []
-        importedTypes[importedModules[split[0]]].append({
+        if split[0] not in imported.get_modules():
+            raise Exception("Using type from module that is not defined \"{}\"".format(split[0]))
+        module_name = imported.get_module(split[0])
+        imported.add_type(module_name, {
             "type_name": type_name,
             "type": split[1]
         })
-    elif type_name not in allowed_types and type_name not in types:
-        raise Exception("Unsupported type {} used".format(type_name))
+    elif type_name not in ALLOWED_TYPES and type_name not in types:
+        raise Exception("Unsupported type \"{}\" used".format(type_name))
     if type_name == "string" and "pattern" in value:
         type_name = {
             "leaf-type": type_name,
@@ -180,11 +116,11 @@ def extract_type_statements(generated, key, value):
     generated["leaf-type"] = type_name
 
 
-def handleTypedef(typedefs):
+def handle_typedef(typedefs):
     if isinstance(typedefs, dict):
         import_type = typedefs["type"]["@name"]
-        if import_type not in allowed_types and import_type not in types:
-            raise Exception("Unsupported type in imported type {}".format(import_type))
+        if import_type not in ALLOWED_TYPES and import_type not in types:
+            raise Exception("Unsupported type in imported type \"{}\"".format(import_type))
         converted = {
             "leaf-type": typedefs["type"]["@name"]
         }
@@ -197,39 +133,39 @@ def handleTypedef(typedefs):
         types[typedefs["@name"]] = converted
     elif isinstance(typedefs, list):
         for item in typedefs:
-            handleTypedef(item)
+            handle_typedef(item)
 
 
-def processNode(generated, key, value):
+def process_node(generated, key, value, imported):
     if isinstance(value, dict):
         inner_key = value["@name"]
         if "mandatory" in value and value["mandatory"]["@value"] == "true":
             if not ("mandatory" in generated):
                 generated["mandatory"] = []
             generated["mandatory"].append(inner_key)
-        generated["map"][inner_key] = convert(value, key)
+        generated["map"][inner_key] = convert(value, imported, key)
     elif isinstance(value, list):
         for inner_value in value:
-            processNode(generated, key, inner_value)
+            process_node(generated, key, inner_value, imported)
 
 
-def convert(level, object_type=None):
+def convert(level, imported, object_type=None):
     generated = {}
     changed_level = level
     if len(changed_level) == 1 and "module" in changed_level:
         generated["type"] = "module"
         changed_level = level["module"]
         if "import" in changed_level:
-            handle_import(changed_level["import"])
+            handle_import(changed_level["import"], imported)
         if "typedef" in changed_level:
-            handleTypedef(changed_level["typedef"])
+            handle_typedef(changed_level["typedef"])
     if object_type is not None:
         generated["type"] = object_type
     generated["map"] = {}
     for key, value in changed_level.items():
-        extract_uci_statements(generated, key, value)
+        extract_uci_statements(generated, key, value, imported)
         if key == "type":
-            extract_type_statements(generated, key, value)
+            extract_type_statements(generated, key, value, imported)
         if key == "key":
             to_be_split = value["@value"]
             generated["keys"] = to_be_split.split()
@@ -239,27 +175,27 @@ def convert(level, object_type=None):
             to_be_split = value["@value"]
             generated["unique"] = to_be_split.split()
         if key in ["container", "leaf", "leaf-list", "list"]:
-            processNode(generated, key, value)
+            process_node(generated, key, value, imported)
     return generated
 
 
-def processImportedTypes(args):
-    for key, value in importedTypes.items():
+def process_imported_types(args, imported):
+    for key, value in imported.get_types().items():
         with open(os.path.join(args.yin_dir, key + ".yin")) as file:
             data = file.read()
             mod = convert_yin_to_json(data)["module"]
             if "typedef" not in mod:
-                raise Exception("Type is not declared in module")
+                raise Exception("Type is not declared in module \"{}\"".format(mod))
             typedef = mod["typedef"]
             if isinstance(typedef, list):
                 for item in typedef:
                     for val in value:
                         if item["@name"] == val["type"]:
                             import_type = item["type"]["@name"]
-                            if import_type not in allowed_types and import_type not in types:
+                            if import_type not in ALLOWED_TYPES and import_type not in types:
                                 raise Exception("Unsupported type in imported type {}".format(import_type))
                             converted = {
-                                "leaf-type": item["type"]["@name"]
+                                "leaf-type": import_type
                             }
                             if converted["leaf-type"] == "string" and "pattern" in item["type"]:
                                 converted["pattern"] = []
@@ -290,16 +226,17 @@ def main():
         with open(file) as yin:
             data = yin.read()
             js = convert_yin_to_json(data)
-            js = convert(js)
+            imported = Imported()
+            js = convert(js, imported)
             modules.append((os.path.basename(file).split('.')[0], js))
 
-    processImportedTypes(args)
+    process_imported_types(args, imported)
 
     if not os.path.exists(args.output):
         os.makedirs(args.output)
 
     with open(os.path.join(args.output, "yang.h"), "w+") as out:
-        rendered = c.render(modules=modules, types=types)
+        rendered = header_file.render(modules=modules, types=types, ALLOWED_TYPES=ALLOWED_TYPES)
         out.write(rendered)
 
 
