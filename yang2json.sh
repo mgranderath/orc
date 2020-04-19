79ac861c98dc24dd296bfcbe1aab4e24ccf288cf
Dushan Terzikj <dusanterzic87@gmail.com>
2020-04-20 00:42:37 +0200

    Update lmapd module with groupings and add conversion bash script

---
+++ yang2json.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+declare -a f_array=("openwrt-uci-extension" "ietf-yang-types" "ietf-inet-types" "ietf-netconf-acm" "ietf-lmap-common" "lmapd" "restconf-example")
+
+for val in ${f_array[@]}; do
+    echo "pyang -f yin ./yang/$val.yang -p ./yang -o ./yin/$val.yin"
+    pyang -f yin ./yang/$val.yang -p ./yang -o ./yin/$val.yin
+done
+
+all_files=""
+
+for val in ${f_array[@]}; do
+    all_files+="./yin/$val.yin "
+done
+
+echo "python3 ./yin2json/yin2json.py -y ./yin -o ./generated $all_files"
+python3 ./yin2json/yin2json.py -y ./yin -o ./generated $all_files
